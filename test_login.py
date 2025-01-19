7d5ba87beb9b0714e716e9a7e435d01a0e4ab80a
Yauheni-Aliakseyenka <alekseenka.evv@gmail.com>
2025-01-19 15:30:10 +0300

    add browser setup

--- test_login.py
+++ test_login.py
@@ -1,13 +1,35 @@
-from playwright.sync_api import Page, expect
+import pytest
+from playwright.sync_api import Page, Playwright
 
 
-def test_super_admin_login(page):
+@pytest.fixture
+def setup_browser(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    yield page
+    context.close()
+    browser.close()
+
+
+def test_super_admin_login(setup_browser: Page):
+    page = setup_browser
     page.goto("https://staging-app.pharosiq.com/signin")
 
-    page.fill("name='email'", "sa1@gmail.com")
-    page.fill("name='password'", "Ed91460b")
+    page.fill("[name='email']", "sa1@gmail.com")
+    page.fill("[name='password']", "Ed91460b")
 
     page.get_by_role("button", name="Log In").click()
+
+    assert page.wait_for_selector("._d0dff29"), "Dashboard is not loaded"
+
+    assert page.is_visible(".daae4068"), "Not a super admin"
+
+    assert page.is_visible("#recharts_measurement_span"), "Message about wrong email or password"
+
+
+
+
 
 
 
