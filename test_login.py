f79e3fe800e090959e1f811de310775287737d12
Yauheni-Aliakseyenka <alekseenka.evv@gmail.com>
2025-01-24 18:12:18 +0300

    add validation tests

--- test_login.py
+++ test_login.py
@@ -25,8 +25,6 @@ def test_super_admin_login(setup_browser: Page):
 
     assert page.is_visible(".daae4068"), "Not a super admin"
 
-    assert page.is_visible("#recharts_measurement_span"), "Message about wrong email or password"
-
 
 def test_admin_login(setup_browser: Page):
     page = setup_browser
@@ -70,5 +68,56 @@ def test_company_user(setup_browser: Page):
     assert page.locator("._a59591b").count() == 3, "Not a company user"
 
 
+@pytest.mark.parametrize(
+    "email, password",
+    [
+        ("sa1@gmail.com", "111"),
+        ("sa1@gmail.co", "Ed91460b"),
+        ("sa1@gmail.co", "111")
+    ]
+)
+def test_invalid_email(setup_browser: Page, email, password):
+    page = setup_browser
+    page.goto("https://staging-app.pharosiq.com/signin")
+
+    page.fill("[name='email']", email)
+    page.fill("[name='password']", password)
+
+    page.get_by_role("button", name="Log In").click()
+
+    assert page.wait_for_selector(".f6d8d3ec"), "Error toaster is not displayed"
+
+
+@pytest.mark.parametrize(
+    "email, password, error_text",
+    [
+        ("sa1@gmail.com", "", "Please specify Password"),
+        ("", "Ed91460b", "Please specify Email"),
+        ("", "", "Please specify Email")
+    ]
+)
+def test_required_fields_validation(setup_browser: Page, email, password, error_text):
+    page = setup_browser
+    page.goto("https://staging-app.pharosiq.com/signin")
+
+    page.fill("[name='email']", email)
+    page.fill("[name='password']", password)
+
+    page.get_by_role("button", name="Log In").click()
+
+    assert page.text_content(".e859cbeb") == error_text, "Error message is not displayed"
+
+
+def test_email_format_validation(setup_browser: Page):
+    page = setup_browser
+    page.goto("https://staging-app.pharosiq.com/signin")
+
+    page.fill("[name='email']", "sa1gmail.com")
+    page.fill("[name='password']", "Ed91460b")
+
+    page.get_by_role("button", name="Log In").click()
+
+    assert page.text_content(".e859cbeb") == "Please enter the email address in the format *@*.*", \
+        "Error message is not displayed"
 
 
