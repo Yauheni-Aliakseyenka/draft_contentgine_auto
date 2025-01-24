78fcf721bb84fdc57517109c503584119289d39a
Yauheni-Aliakseyenka <alekseenka.evv@gmail.com>
2025-01-24 13:12:31 +0300

    add new roles tests

--- test_login.py
+++ test_login.py
@@ -28,7 +28,46 @@ def test_super_admin_login(setup_browser: Page):
     assert page.is_visible("#recharts_measurement_span"), "Message about wrong email or password"
 
 
+def test_admin_login(setup_browser: Page):
+    page = setup_browser
+    page.goto("https://staging-app.pharosiq.com/signin")
 
+    page.fill("[name='email']", "a1@gmail.com")
+    page.fill("[name='password']", "mF5950J8")
+
+    page.get_by_role("button", name="Log In").click()
+
+    assert page.wait_for_selector("._d0dff29"), "Dashboard is not loaded"
+
+    assert page.is_visible("._409794c"), "Not an admin"
+
+
+def test_guest_user(setup_browser: Page):
+    page = setup_browser
+    page.goto("https://staging-app.pharosiq.com/signin")
+
+    page.fill("[name='email']", "y.aliakseyenka+guest@paralect.com")
+    page.fill("[name='password']", "136495aleX")
+
+    page.get_by_role("button", name="Log In").click()
+
+    assert page.wait_for_selector("._d0dff29"), "Dashboard is not loaded"
+
+    assert page.is_visible("._f2b5494"), "Not a guest user"
+
+
+def test_company_user(setup_browser: Page):
+    page = setup_browser
+    page.goto("https://staging-app.pharosiq.com/signin")
+
+    page.fill("[name='email']", "cu1@gmail.com")
+    page.fill("[name='password']", "0B53iApk")
+
+    page.get_by_role("button", name="Log In").click()
+
+    assert page.wait_for_selector("._d0dff29"), "Dashboard is not loaded"
+
+    assert page.locator("._a59591b").count() == 3, "Not a company user"
 
 
 
